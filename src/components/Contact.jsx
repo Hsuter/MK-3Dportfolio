711e21ef6941665845cae10664d7c9de2e6f029a
Harry Suter <harry.suter.hs@gmail.com>
2023-05-09 14:17:08 +0300

    3d

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,7 +64,7 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden md:items-none  `}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
@@ -125,19 +125,9 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
       >
-        <ErrorBoundary
-          fallback={
-            <img
-              src={earthFall}
-              alt="globe"
-              className="ml-28 md:w-[500px] w-[300px]"
-            />
-          }
-        >
-          <ComputersCanvas />
-        </ErrorBoundary>
+        <img src={earthFall} alt="globe" className=" md:w-[500px] w-[300px] " />
       </motion.div>
     </div>
   );
